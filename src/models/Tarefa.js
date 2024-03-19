96a7a2c20c07d78b50aaff2ed19dc9702bc9bbd2
Andres Cristian <cristian.yapiticona@gmail.com>
2024-03-19 10:54:58 -0300

    Update Tarefa.js

--- src/models/Tarefa.js
+++ src/models/Tarefa.js
@@ -1,7 +1,5 @@
 const {model, Schema} = require('mongoose')
 
-//Criar um Objeto --> Modelo para coleção
-//IMPORTANTE: Nomes dos Atributos serem iguais ao Objeto recebido pelo main.js
 const tarefaSchema = new Schema({
     nome: {
         type: String
@@ -14,9 +12,4 @@ const tarefaSchema = new Schema({
     }
 })
 
-
-
-//Exportar o Schema -> main.js
 module.exports = model('Contatos', tarefaSchema)
-
-
